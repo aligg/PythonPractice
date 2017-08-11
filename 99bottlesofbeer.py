6dc052384e48eb7549f116b1e615d2b21a3ec8e0
Ali G <ali.glenesk@gmail.com>
2017-08-10 17:17:37 -0700

    commit
    commit

--- 99bottlesofbeer.py
+++ 99bottlesofbeer.py
@@ -2,6 +2,11 @@
 bottlenumber= 99
 bottlenumbernext=98
 while bottlenumber >= 1:
-    print("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around, %d bottles of beer on the wall" % (bottlenumber, bottlenumber, bottlenumbernext))
+    if bottlenumber == 1:
+        print("%d bottle of beer on the wall, %d bottle of beer. Take one down, pass it around, %d bottles of beer on the wall" % (bottlenumber, bottlenumber, bottlenumbernext)) 
+    elif bottlenumber == 2:
+        print("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around, %d bottle of beer on the wall" % (bottlenumber, bottlenumber, bottlenumbernext))
+    else:
+        print("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around, %d bottles of beer on the wall" % (bottlenumber, bottlenumber, bottlenumbernext))
     bottlenumber = bottlenumber -1
     bottlenumbernext = bottlenumbernext -1
